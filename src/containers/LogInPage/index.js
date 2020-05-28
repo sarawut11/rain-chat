b1dd0570581d021567ba529d7e40221721b9257d
dhgarmo <ghostqueen@Ghostqueens-iMac.local>
2020-05-28 18:05:16 +0200

    Landing page completed

--- src/containers/LogInPage/index.js
+++ src/containers/LogInPage/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'antd';
 import Request from '../../utils/request';
 import Modal from '../../components/Modal';
 import notification from '../../components/Notification';
 import SignInSignUp from '../../components/SignInSignUp';
+import { landingDescription } from '../../constants/login';
 import './index.scss';
 
 class LogIn extends Component {
@@ -80,12 +82,69 @@ class LogIn extends Component {
 
   render() {
     const { visible } = this.state.modal;
+    const {
+      welcomeTitle,
+      welcomeText,
+      title1,
+      text1,
+      title2,
+      text2,
+      title3,
+      text3,
+    } = landingDescription;
     return (
       <div className="login">
         <Modal title="Alert" visible={visible} confirm={this.confirm} hasConfirm>
           <p className="content">You have successfully logged in</p>
         </Modal>
-        <SignInSignUp setValue={this.setValue} isLogin />
+
+        <Row justify="space-around" align="middle" className="login-container">
+          <Col xs={24} sm={24} md={24} lg={10} className="login-form-container">
+            <Row justify="center" align="middle" style={{ height: '100%' }}>
+              <SignInSignUp setValue={this.setValue} isLogin />
+            </Row>
+          </Col>
+
+          <Col xs={24} sm={24} md={24} lg={14} className="login-description-container">
+            <Row justify="center" align="middle">
+              <Col span={3} />
+              <Col span={18}>
+                <Row justify="center" align="middle" gutter={[0, 20]}>
+                  <Col span={24}>
+                    <Row justify="center">
+                      <img src="../../assets/vitae-logo.png" alt="vitae-logo" />
+                    </Row>
+                  </Col>
+
+                  <Col span={24}>
+                    <h1>{welcomeTitle}</h1>
+                    <p>{welcomeText}</p>
+                  </Col>
+
+                  <Col span={24}>
+                    <h2>{title1}</h2>
+                    <p>{text1}</p>
+                  </Col>
+
+                  <Col span={24}>
+                    <h2>{title2}</h2>
+                    <p>{text2}</p>
+                  </Col>
+
+                  <Col span={24}>
+                    <h2>{title3}</h2>
+                    <p>{text3}</p>
+                  </Col>
+                </Row>
+              </Col>
+              <Col span={3} />
+              {/* <div className="login-description-box">
+              </div> */}
+            </Row>
+          </Col>
+        </Row>
+
+        <span className="login-logo-text">Vitae Rain Chat</span>
       </div>
     );
   }
