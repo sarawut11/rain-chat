811703013f874e2d49605c296a775ea89b908eb6
Fire King <firek7731@gmail.com>
2020-06-06 00:43:55 +1000

    testing

--- server/src/app/context/RainContext.ts
+++ server/src/app/context/RainContext.ts
@@ -24,6 +24,7 @@ export class RainContext {
     // | | hour
     // | minute
     // second ( optional )
+    let admin;
     const rainTimeInterval = configs.rain.rain_time_interval / 1000;
     RainContext.rainJob = new CronJob(`*/${rainTimeInterval} * * * * *`, async () => {
       try {
