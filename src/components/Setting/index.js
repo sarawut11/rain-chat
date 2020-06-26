5d2acfbe8975c9f7fa986bf868d3220078fb57a6
sarawut <sanit.sa@outlook.com>
2020-06-26 13:05:49 +1000

    fix lint warning issue

--- src/components/Setting/index.js
+++ src/components/Setting/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Modal } from 'antd';
 import { withRouter } from 'react-router-dom';
