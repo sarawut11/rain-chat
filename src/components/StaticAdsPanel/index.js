291b5efa7f7af0aee1a367378ac483d84010733c
chaingergar <garmoniart@gmail.com>
2020-07-17 01:29:18 +0800

    Unused code clean

--- src/components/StaticAdsPanel/index.js
+++ src/components/StaticAdsPanel/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Button, Row, Col } from 'antd';
 import { setStaticAdsAction } from '../../redux/actions/staticAdsAction';
