e4c7815e254315f96ed72fe25138a589bc3f0787
chaingergar <garmoniart@gmail.com>
2020-07-21 11:29:15 +0800

    Unused code clean

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -102,51 +102,6 @@ export default class SignUp extends Component {
 
     return (
       <div className="sign-up-container">
-        {/* {this.state.showSpinner && <Spinner />}
-        <div className="userAvatarWrapper">
-          <UserAvatar name={username || 'U'} size="100" color="#44C97D" />
-        </div>
-        <div className="center">
-          <div className="center">
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-              placeholder="Full Name"
-            />
-          </div>
-          <div className="center">
-            <input
-              type="text"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-              placeholder="Email"
-            />
-          </div>
-        </div>
-        <div className="center">
-          <input
-            type="text"
-            name="username"
-            value={username}
-            onChange={this.handleChange}
-            placeholder="Username"
-          />
-        </div>
-        <div className="center">
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={this.handleChange}
-            placeholder="Password"
-          />
-        </div>
-        <div className="center">
-          <input type="button" onClick={this.handleClick} value="Register" />
-        </div> */}
         <Card
           title="Sign Up"
           bordered={false}
