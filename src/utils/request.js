930d8a7e5fd5afd0199e89ae87171dfd9b7981c8
sarawut <sanit.sa@outlook.com>
2020-08-20 20:01:10 +1000

    add fixed url rather than use .env in frontend

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { notification } from 'antd';
 
 export default class Request {
-  static apiUrl = process.env.API_URL;
+  static apiUrl = 'https://vitaerain.chat';
 
   static axiosConfigInit() {
     axios.defaults.baseURL = this.apiUrl;
